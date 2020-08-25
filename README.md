3e88174f28606ca126c993e2198ab028791f47e5
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-08-25 16:48:03 -0700

    Update to address portion

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 #### _A web application that will take take in user input and save the data in objects. Created 8.25.20_
 
-#### By _**Chloe Hellberg**_
+#### By _**Chloe Hellberg and Garrett Brown**_
 
 ## Description: Behavior Driven Development/Spec
 
@@ -33,4 +33,42 @@ _This application utilizes HTML/CSS, Bootstrap, JavaScript and jQuery._
 
 *MIT License*
 
-Copyright (c) 2020 **_Chloe Hellberg_**
+Copyright (c) 2020 **_Chloe Hellberg and Garrett Brown_**
+
+
+
+
+-Add functionality to record and display a contact's email address
+-Add functionality to record and display a contact's physical address
+-
+
+-What is a contructor? What is a prototype?
+  
+  -A constructor is a function that can be invoked using the "new" keyword
+  to create new objects
+  -Good practice to name constructor functions with an upper-case first letter
+  -A constructor is a blueprint that specifies how to create an object
+    -i.e. let testGreeting2 = new String("Hello!);
+    -i.e. function Dog(name,colors,age) {
+              this.name = name;
+              this.colors = colors;
+              this.age = age;
+               }
+        Then to create a new dog, we can do the following:
+            let myPuppy = new Dog("Ernie", ["brown", "black"], 3);
+  
+  -A prototype is just an object from which other objects inherit methods
+    -i.e. String.prototype.addExcitement = function() { return this + !!!!" };
+  -All JavaScript objects inherit properties and methods from a prototype
+
+
+-When would you use a Constructor, and when would you use a Prototype?
+  -You will use a constructor when you want to create a "blueprint" for an object you want to create
+  -You will use a prototype when you want to create a method and call it on your objects. Or, I guess
+    it is an object and you are creating (that has a function) and can be called on your other objects if you want to do something with them.
+
+
+-The this keyword
+  -The things called this is the object that "owns" the code
+  -The value of this, when used in an object, is the object itself
+  -In a constructor function this does not have a value. It is a substitute for the new object. The value of this will become the new object when a new object is created
--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,10 @@ AddressBook.prototype.addContact = function(contact) {
   this.contacts.push(contact);
 }
 
+// Contact.prototype.addAddress = function(addAddress) {
+//   this.address.push(addAddress);
+// }
+
 AddressBook.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
@@ -16,7 +20,7 @@ AddressBook.prototype.assignId = function() {
 
 AddressBook.prototype.findContact = function(id) {
   for (let i=0; i< this.contacts.length; i++) {
-    if (this.contacts[i]) {
+    if (this.contacts[i]) {     // <-- This line is new!
       if (this.contacts[i].id == id) {
         return this.contacts[i];
       }
@@ -27,7 +31,7 @@ AddressBook.prototype.findContact = function(id) {
 
 AddressBook.prototype.deleteContact = function(id) {
   for (let i=0; i< this.contacts.length; i++) {
-    if (this.contacts[i]) {
+    if (this.contacts[i]) {     // <-- This line is new!
       if (this.contacts[i].id == id) {
         delete this.contacts[i];
         return true;
@@ -37,7 +41,12 @@ AddressBook.prototype.deleteContact = function(id) {
   return false;
 }
 
-AddressBook.prototype.updateContact = function(id, first, last, number) {
+// AddressBook.prototype.updateContact = function(id) {
+//   contact.id = this.assignId();
+//   this.contacts.push(contact);
+// }
+
+AddressBook.prototype.updateContact = function(id, first, last, number, email, /*home*/ ) {
   for (let i = 0; i < this.contacts.length; i++) {
     if (this.contacts[i]) {
       if (this.contacts[i].id == id) {
@@ -50,23 +59,45 @@ AddressBook.prototype.updateContact = function(id, first, last, number) {
         if (number != "") {
           this.contacts[i].phoneNumber = number;
         }
+        if (email != "") {
+          this.contacts[i].email = email;
+        }
+       /* if (home != "") {
+          this.contacts[i].home = home;
+        }*/
+        if (type != "") {
+          this.contacts[i].type = type;
+        }
+        if (address != "") {
+          this.contacts[i].address = address;
+        }
       }
     }
   }
 }
 
-
-/// Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber) {
+// Business Logic for Contacts ---------
+function Contact(firstName, lastName, phoneNumber, email, type, address) {
   this.firstName = firstName,
   this.lastName = lastName,
-  this.phoneNumber = phoneNumber
+  this.phoneNumber = phoneNumber,
+  this.email = email,
+  // alert(address);
+  this.address = { addressType: type, addressName: address}
 }
+
+// contact.address.addressType = 
+// function Address(addressType, streetAddress) {
+//   this.addressType = addressType,
+//   this.streetAddress = streetAddress
+
+// }
+
+
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
-
 
 
 // User Interface Logic ---------
@@ -81,21 +112,26 @@ function displayContactDetails(addressBookToDisplay) {
   contactsList.html(htmlForContactInfo);
 };
 
+
 function showContact(contactId) {
-  var contact = addressBook.findContact(contactId);
+  const contact = addressBook.findContact(contactId);
   $("#show-contact").show();
   $(".first-name").html(contact.firstName);
   $(".last-name").html(contact.lastName);
-  $(".phone-number").html(contact.address);
-  var buttons = $("#buttons");
+  $(".phone-number").html(contact.phoneNumber);
+  $(".email-address").html(contact.email);
+  //$(".home-address").html(contact.home);
+  let address = contact.address.addressType + ": " + contact.address.addressName;
+  $(".address-ultimate").html(address);
+
+  let buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
 }
 
-
 function attachContactListeners() {
   $("ul#contacts").on("click", "li", function() {
-    showContact(this.id);
+    showContact(this.id); 
   });
   $("#buttons").on("click", ".deleteButton", function() {
     addressBook.deleteContact(this.id);
@@ -105,17 +141,34 @@ function attachContactListeners() {
 };
 
 $(document).ready(function() {
-  attachContactListeners();
+  attachContactListeners(); 
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
-    let inputtedFirstName = $("input#new-first-name").val();
-    let inputtedLastName = $("input#new-last-name").val();
-    let inputtedPhoneNumber = $("input#new-phone-number").val();
+    const inputtedFirstName = $("input#new-first-name").val();
+    const inputtedLastName = $("input#new-last-name").val();
+    const inputtedPhoneNumber = $("input#new-phone-number").val();
+    const inputtedEmailAddress= $("input#new-email-address").val();
+    //const inputtedHomeAddress= $("input#new-home-address").val();
+    const inputtedAddressType= $("input#new-address-type").val();
+    const inputtedAddressUltimate= $("input#new-address").val();
+
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-phone-number").val("");
-    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    $("input#new-email-address").val("");
+    //$("input#new-home-address").val(""); 
+    $("input#new-address-type").val("");
+    $("input#new-address").val("");
+
+    // alert("in");
+    //let newAddress = new Address(inputtedAddressType, inputtedAddressUltimate);
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmailAddress, inputtedAddressType, inputtedAddressUltimate);
+
+   
     addressBook.addContact(newContact);
-    displayContactDetails(addressBook);
-  })
-})
+    // console.log(addressBook.contacts);
+    // newContact.addAddress(newAddress);
+    // console.log(newContact.address);
+    displayContactDetails(addressBook); 
+  });
+});
